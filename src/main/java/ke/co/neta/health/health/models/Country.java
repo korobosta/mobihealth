2542f532391b8d07e99347fe9b679b8c1cd167d8
Kevin Ongulu <kevinkorobosta@gmail.com>
2024-05-24 16:14:39 +0300

    Country.java,Location.java update

--- src/main/java/ke/co/neta/health/health/models/Country.java
+++ src/main/java/ke/co/neta/health/health/models/Country.java
@@ -16,15 +16,51 @@ public class Country {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name="name",length = 4,nullable = false,unique=true)
+    @Column(name="name",length = 100,nullable = false,unique=true)
     private String name;
 
-    @Column(name="code",length = 4,nullable = false,unique=true)
-    private int code;
+    @Column(name="nicename",length = 100,nullable = false,unique=true)
+    private String niceName;
+
+    @Column(name="phonecode",length = 5,nullable = false,unique=true)
+    private int phoneCode;
+
+    @Column(name="numcode",length = 5,nullable = false,unique=true)
+    private int numCode;
+
+    @Column(name="iso",length = 2,nullable = false,unique=true)
+    private String iso;
+
+    @Column(name="iso3",length = 3,nullable = false,unique=true)
+    private String iso3;
 
     @CreationTimestamp(source = SourceType.DB)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp(source = SourceType.DB)
     private LocalDateTime updatedAt;
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setNiceName(String niceName){
+        this.niceName = niceName;
+    }
+
+    public void setPhoneCode(int phoneCode){
+        this.phoneCode = phoneCode;
+    }
+
+    public void setNumCode(int numCode){
+        this.numCode = numCode;
+    }
+
+    public void setIso(String iso){
+        this.iso = iso;
+    }
+
+    public void setIso3(String iso3){
+        this.iso3 = iso3;
+    }
 }
--- src/main/java/ke/co/neta/health/health/models/Location.java
+++ src/main/java/ke/co/neta/health/health/models/Location.java
@@ -1,5 +1,33 @@
 package ke.co.neta.health.health.models;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SourceType;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.*;
+
 public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="name",length = 100)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_location_id",nullable=true)
+    private Location location;
+
+    @CreationTimestamp(source = SourceType.DB)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp(source = SourceType.DB)
+    private LocalDateTime updatedAt;
     
 }
